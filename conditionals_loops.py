556433447d07c05aa80e4c465a1f8703a69a09f0
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2025-02-05 01:16:27 +0800

    add guessing game with loop

--- conditionals_loops.py
+++ conditionals_loops.py
@@ -10,3 +10,17 @@ num = 0
 while num < 10:
     print(num)
     num = num + 1
+    
+#Guessing game 
+
+secret_number = 5 
+
+guess = int(input("Enter your guess number: "))
+
+while guess != secret_number:
+    if guess < secret_number:
+        print("Too low!")
+    else:
+        print("Too high!")
+        
+print("Congratulations! You have guessed the number")
