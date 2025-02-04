58de719f8b3830b79ba512319e813a0f9b12e663
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2025-02-05 01:00:44 +0800

    add input variable

--- variables.py
+++ variables.py
@@ -1,11 +1,14 @@
-x = 5
-y = 10 
-name = "Shin"
-is_student = True
-father_name = "Lim"
-age = 21
+# x = 5
+# y = 10 
+# name = "Shin"
+# is_student = True
+# father_name = "Lim"
+# age = 21
 
-print(x + y)
-print(x - y)
-print(y * x)
-print(name + " "+ father_name + " is" + " " + str(age) + " years old")
+# print(x + y)
+# print(x - y)
+# print(y * x)
+# print(name + " "+ father_name + " is" + " " + str(age) + " years old")
+
+name = input("What is your name?")
+print("Welcome " + name)
