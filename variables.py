affe590dfd5411edf12566cf12a0af1cf55344a7
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2025-02-05 00:56:08 +0800

    add age

--- variables.py
+++ variables.py
@@ -3,8 +3,9 @@ y = 10
 name = "Shin"
 is_student = True
 father_name = "Lim"
+age = 21
 
 print(x + y)
 print(x - y)
 print(y * x)
-print(name + " "+ father_name)
+print(name + " "+ father_name + " is" + " " + str(age) + " years old")
