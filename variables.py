ea822f229e54df4ed0bd6295204d1601da68ab69
Shin I. Lim <92615780+ShinayLim@users.noreply.github.com>
2025-02-05 01:05:14 +0800

    add age

--- variables.py
+++ variables.py
@@ -10,5 +10,9 @@
 # print(y * x)
 # print(name + " "+ father_name + " is" + " " + str(age) + " years old")
 
-name = input("What is your name?")
+name = input("What is your name? ")
 print("Welcome " + name)
+
+age = input("How old are you? ")
+age_in_five_years = int(age) + 5
+print("You will be " + str(age_in_five_years) + " years old after five years.")
